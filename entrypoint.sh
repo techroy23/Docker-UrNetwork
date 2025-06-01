e4324e5c8705697cb077ff827fa04d123c6594d3
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-06-02 02:25:30 +0800

    Update entrypoint.sh
    added arch detect

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,21 @@ echo " "
 
 sleep 3
 echo "##### Extracting Provider Binary #####"
-tar -xzf latest.tar.gz --strip-components=2 -C /app linux/amd64/provider
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]]; then
+    TARGET_ARCH="amd64"
+elif [[ "$ARCH" == "aarch64" ]]; then
+    TARGET_ARCH="arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+echo "Detected architecture: $TARGET_ARCH"
+tar -xzf latest.tar.gz --strip-components=2 -C /app linux/$TARGET_ARCH/provider
 chmod +x /app/provider
+
 rm latest.tar.gz
 echo " "
 
