f2805a6629071616dbc318d356887ae9f3bbab0d
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-06-12 11:46:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,57 +36,19 @@ if [[ ! -x "./provider" ]]; then
 fi
 
 # -----------------------------------------------------------------------------
-# Proxy Configuration (if applicable)
-# -----------------------------------------------------------------------------
-if [ -n "$PROXY" ]; then
-    echo "##### Configuring ProxyChains #####"
-    protocol=$(echo "$PROXY" | awk -F'://' '{print $1}')
-    auth=$(echo "$PROXY" | awk -F'@' '{print $1}' | awk -F'://' '{print $2}')
-    username=$(echo "$auth" | awk -F':' '{print $1}')
-    password=$(echo "$auth" | awk -F':' '{print $2}')
-    address=$(echo "$PROXY" | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
-    port=$(echo "$PROXY" | awk -F':' '{print $NF}')
-    if [[ -z "$protocol" || -z "$username" || -z "$password" || -z "$address" || -z "$port" ]]; then
-        echo "Error: Unable to parse proxy details correctly!"
-        exit 1
-    fi
-    cat <<EOF > /etc/proxychains.conf
-strict_chain
-proxy_dns
-tcp_read_time_out 30000
-tcp_connect_time_out 30000
-
-[ProxyList]
-$protocol $address $port $username $password
-EOF
-    echo " "
-    echo "Using ProxyChains..."
-    sleep 3
-    echo " "
-    ipchecker=$(proxychains curl -s ifconfig.me)
-    echo "Your proxied IP: $ipchecker"
-    echo " "
-    proxychains4 ./provider auth --user_auth="${USER_AUTH}" --password="${PASSWORD}"
-    echo " "
-    sleep 3
-    proxychains4 ./provider provide &
-else
-    echo " "
-    echo "No proxy set. Running app directly..."
-    sleep 3
-    echo " "
-    ./provider auth --user_auth="${USER_AUTH}" --password="${PASSWORD}"
-    sleep 3
-    echo " "
-    proxychains4 ./provider provide &
-fi
-
-# -----------------------------------------------------------------------------
 # Keep Script Running Indefinitely to Prevent Premature Container Exit
 # -----------------------------------------------------------------------------
 sleep 3
 echo " "
 echo "##### Running Indefinitely #####"
+echo " "
+echo "No proxy set. Running app directly..."
+sleep 3
+echo " "
+./provider auth --user_auth="${USER_AUTH}" --password="${PASSWORD}"
+sleep 3
+echo " "
+proxychains4 ./provider provide &
 echo " "
 
 while true; do sleep 3600; done
