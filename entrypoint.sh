04e64dc6ccb7b66cba2ca4710db20c2355d47b25
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-07-01 13:36:59 +0800

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,15 +41,22 @@ while true; do
     echo "Checking for existing JWT..."
     JWT_FILE="/root/.urnetwork/jwt"
     if [ ! -f "$JWT_FILE" ]; then
-        echo "JWT not found. Authenticating..."
+        echo " "
+        echo " ############################ "
+        echo " ### Recreating JWT Cache ### "
+        echo " ############################ "
+        echo " "
         ./provider auth --user_auth="${USER_AUTH}" --password="${PASSWORD}" -f
     fi
 
-    echo "Starting provider..."
+    echo " "
+    echo " ################################# "
+    echo " ### Starting Provider Service ### "
+    echo " ################################# "
+    echo " "
     ./provider provide &
     PROVIDER_PID=$!
 
-    # Generate random number between 1 and 15
     if command -v shuf >/dev/null 2>&1; then
         RANDOM_MINUTES=$(shuf -i 1-15 -n 1)
     else
@@ -57,14 +64,31 @@ while true; do
     fi
 
     TOTAL_WAIT=$((3600 + RANDOM_MINUTES * 60))
-    echo "Sleeping for $((TOTAL_WAIT / 60)) minutes before restarting provider..."
+    echo " "
+    echo " ############################################################# "
+    echo " ### Sleeping for $((TOTAL_WAIT / 60)) minutes before restarting provider... ### "
+    echo " ############################################################# "
+    echo " "
     sleep "$TOTAL_WAIT"
 
-    echo "Killing provider process..."
+    echo " "
+    echo " ################################### "
+    echo " ### Killing provider process... ### "
+    echo " ################################### "
+    echo " "
     pkill -f "./provider" || echo "No matching processes found."
 
-    echo "Deleting JWT token..."
+    echo " "
+    echo " ############################# "
+    echo " ### Deleting JWT token... ### "
+    echo " ############################# "
+    echo " "
     rm -f "$JWT_FILE"
 
-    echo "Restarting loop..."
+    echo " "
+    echo " ########################## "
+    echo " ### Restarting loop... ### "
+    echo " ########################## "
+    echo " "
+
 done
